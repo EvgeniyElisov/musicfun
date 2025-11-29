5ece4b294d5444f6aa5c07c007d65bc70138c987
EvgeniyElisov <zzzzz-123@inbox.ru>
2025-11-29 20:48:30 +0300

    some refactor

--- src/features/playlists/ui/PlaylistsPage/PlaylistItem/PlaylistCover/PlaylistCover.tsx
+++ src/features/playlists/ui/PlaylistsPage/PlaylistItem/PlaylistCover/PlaylistCover.tsx
@@ -1,9 +1,9 @@
-import type { Images } from "@/common/types"
-import { useDeletePlaylistCoverMutation, useUploadPlaylistCoverMutation } from "@/features/playlists/api/playlistsApi"
-import type { ChangeEvent } from "react"
 import defaultCover from "@/assets/images/default-playlist-cover.png";
-import s from "./PlaylistCover.module.css"
-import { toast } from "react-toastify";
+import type { Images } from "@/common/types";
+import { errorToast } from "@/common/utils";
+import { useDeletePlaylistCoverMutation, useUploadPlaylistCoverMutation } from "@/features/playlists/api/playlistsApi";
+import type { ChangeEvent } from "react";
+import s from "./PlaylistCover.module.css";
 
 type Props = {
   playlistId: string
@@ -25,14 +25,11 @@ export const PlaylistCover = ({ images, playlistId }: Props) => {
     if (!file) return
  
     if (!allowedTypes.includes(file.type)) {
-      toast('Only JPEG, PNG or GIF images are allowed', { type: 'error', theme: 'colored' })
+      errorToast('Only JPEG, PNG or GIF images are allowed')
     }
  
      if (file.size > maxSize) {
-      toast(`The file is too large (max. ${Math.round(maxSize / 1024)} KB)`, {
-        type: 'error',
-        theme: 'colored',
-      })
+      errorToast(`The file is too large (max. ${Math.round(maxSize / 1024)} KB)`)
     }
  
     uploadCover({ playlistId, file })
